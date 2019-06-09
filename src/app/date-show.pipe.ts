61efb755be2c56d47ceaccb9e172b20f6db5d738
Vivvianne Kimani <ndiahav@gmail.com>
2019-06-09 17:56:51 +0300

    add function to chane the data format

--- src/app/date-show.pipe.ts
+++ src/app/date-show.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Pipe({
   name: 'dateShow'
 })
 export class DateShowPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(date: Date , format: string = 'dd/MMM/yyyy'): string {
+    date = new Date(date);
+    date.setDate(date.getDate());
+    return new DatePipe('en-US').transform(date, format);
+    
   }
-
 }
+
+
