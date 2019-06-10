788464013625bdbde88986379d720e25aa2588f0
Vivvianne Kimani <ndiahav@gmail.com>
2019-06-10 11:47:06 +0300

    Auto-generated commit

--- src/app/date-show.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateShowPipe } from './date-show.pipe';
-
-describe('DateShowPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateShowPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/repository.ts
+++
@@ -1,9 +0,0 @@
-export class Repository {
-    public reponame: string;
-    public repoDetails: string; 
-
-    constructor(reponame: string, repoDetails: string){
-        this.reponame = reponame, this.repoDetails = repoDetails
-
-    }
-}
